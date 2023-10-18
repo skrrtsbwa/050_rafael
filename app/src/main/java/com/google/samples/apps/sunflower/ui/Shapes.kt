bceb8d9000413f06a57a7e7824de2854b33e029d
skrrtsbwa <rafaeldaniel240104@gmail.com>
2023-10-19 01:02:30 +0700

    UTS pemograman mobile

--- app/src/main/java/com/google/samples/apps/sunflower/ui/Shapes.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/ui/Shapes.kt
@@ -21,16 +21,16 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(
-        topStart = 0.dp,
-        topEnd = 12.dp,
-        bottomStart = 12.dp,
-        bottomEnd = 0.dp
-    ),
-    medium = RoundedCornerShape(
-        topStart = 0.dp,
-        topEnd = 12.dp,
-        bottomStart = 12.dp,
-        bottomEnd = 0.dp
-    )
+        small = RoundedCornerShape(
+                topStart = 32.dp,
+                topEnd = 32.dp,
+                bottomStart = 32.dp,
+                bottomEnd = 32.dp
+        ),
+        medium = RoundedCornerShape(
+                topStart = 32.dp,
+                topEnd = 32.dp,
+                bottomStart = 32.dp,
+                bottomEnd = 32.dp
+        )
 )
